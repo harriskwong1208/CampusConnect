cbf9af3a43b893677fa786bbebdd853999657884
harris <harriskwong120898@gmail.com>
2023-07-11 19:17:03 -0400

    Configured program.cs to use DbContext

--- MVCApi/MVCApi/Data/ApplicationDbContext.cs
+++ MVCApi/MVCApi/Data/ApplicationDbContext.cs
@@ -1,6 +1,14 @@
-﻿namespace MVCApi.Data
+﻿using Microsoft.EntityFrameworkCore;
+using MVCApi.Models;
+
+namespace MVCApi.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext: DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+
+        }
+        public DbSet<Category> categories { get; set; }
     }
 }
--- MVCApi/MVCApi/Program.cs
+++ MVCApi/MVCApi/Program.cs
@@ -1,7 +1,10 @@
+using MVCApi.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer);
 
 var app = builder.Build();
 
