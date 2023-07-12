79aeb481371185ef4628333dc8b1bc25e94ac0a3
harris <harriskwong120898@gmail.com>
2023-07-11 22:33:45 -0400

    Render IEumerable Category obj in Category index view

--- MVCApi/MVCApi/Controllers/CategoryController.cs
+++ MVCApi/MVCApi/Controllers/CategoryController.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MVCApi.Data;
+using MVCApi.Models;
 
 namespace MVCApi.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Category> objCategoryList = _db.categories;
+            return View(objCategoryList);
         }
     }
 }
