3834030e983576f1959fa6c92ea4f51bf82fbca6
harris <harriskwong120898@gmail.com>
2023-07-13 18:03:43 -0400

    Added custom display name, settings, and error message in category create controller

--- MVCApi/MVCApi/Models/Category.cs
+++ MVCApi/MVCApi/Models/Category.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Newtonsoft.Json.Serialization;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace MVCApi.Models
 {
@@ -8,6 +10,8 @@ namespace MVCApi.Models
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1,100,ErrorMessage = "Display Order must be between 1 and 100 only!")]
         public int DisplayOrder { get; set; }
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
     }
